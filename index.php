af8dbe9c809ec865a2bfb878d4fd28889bc234a9
Rahmadhan19 <55820092+Rahmadhan19@users.noreply.github.com>
2020-09-24 17:15:08 +0700

    Update index.php

--- index.php
+++ index.php
@@ -25,89 +25,31 @@
 	</head>
 	<body class="landing">
 
-		<!-- Header -->
-			<header id="header">
-				<h1><a href="index.html">Interphase</a></h1>
-				<nav id="nav">
-					<ul>
-						<li><a href="index.html">Home</a></li>
-						<li><a href="generic.html">Generic</a></li>
-						<li><a href="elements.html">Elements</a></li>
-					</ul>
-				</nav>
-			</header>
-
-		<!-- Banner -->
-			<section id="banner">
-				<h2>This is Interphase</h2>
-				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
-				<ul class="actions">
-					<li>
-						<a href="#" class="button big">Learn More</a>
-					</li>
-				</ul>
-			</section>
-
-		<!-- One -->
-			<section id="one" class="wrapper style1 align-center">
-				<div class="container">
-					<header>
-						<h2>Consectetur adipisicing elit</h2>
-						<p>Lorem ipsum dolor sit amet adipisicing elit. Delectus consequatur sed tempus.</p>
-					</header>
-					<div class="row 200%">
-						<section class="4u 12u$(small)">
-							<i class="icon big rounded fa-clock-o"></i>
-							<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Enim quam consectetur quibusdam magni minus aut modi aliquid.</p>
-						</section>
-						<section class="4u 12u$(small)">
-							<i class="icon big rounded fa-comments"></i>
-							<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laudantium ullam consequatur repellat debitis maxime molestiae.</p>
-						</section>
-						<section class="4u$ 12u$(small)">
-							<i class="icon big rounded fa-user"></i>
-							<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque eaque eveniet, nesciunt molestias. Ipsam, voluptate vero.</p>
-						</section>
-					</div>
-				</div>
-			</section>
-
-		<!-- Two -->
+		<!-- Page -->
 			<section id="two" class="wrapper style2 align-center">
 				<div class="container">
 					<header>
-						<h2>Lorem ipsum dolor sit</h2>
-						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio, autem.</p>
+						<h2>Cloud Computing</h2>
+						<p>Rahmadhan Adinugroho 11180910000046</p>
 					</header>
 					<div class="row">
 						<section class="feature 6u 12u$(small)">
-							<img class="image fit" src="images/pic01.jpg" alt="" />
-							<h3 class="title">Lorem ipsum dolor</h3>
-							<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore ipsa voluptate, quae quibusdam. Doloremque similique, reiciendis sit quibusdam aperiam? Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+							<img class="image fit" src="images/bg1.jpg" alt="" />
+							<h3 class="title">Auditorium Harun Nasution UIN Jakarta</h3>
+							<p>Auditorium Harun adalah tempat yang saklar bagi mahasiswa UIN Jakarta dikarnakan tempat dilaksanakan upacara wisuda bagi mahasiswa. Selain 
+       							 dari itu tempat ini juga sering dilaksanakannya acara penting kampus. Saya pertama kali kesana untuk pembukaan makrab fakultas kesan 
+     							 saya sangat bagus karna ruanganya bersih dan dingin walaupun memuat banyak orang. Serta saya mulai berhayal kapan saya diwisuda disana semoga aja bentar lagi.</p>
 						</section>
 						<section class="feature 6u$ 12u$(small)">
-							<img class="image fit" src="images/pic02.jpg" alt="" />
-							<h3 class="title">Esse, fugiat, in</h3>
-							<p>Natus perspiciatis fugit illum porro iusto fuga nam voluptas minima voluptates deserunt, veniam reiciendis harum repellat necessitatibus. Animi, adipisci qui. Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
-						</section>
-						<section class="feature 6u 12u$(small)">
-							<img class="image fit" src="images/pic03.jpg" alt="" />
-							<h3 class="title">Voluptates, repudiandae, dolor</h3>
-							<p>Voluptatibus repellendus tempora, quia! Consequuntur atque, rerum quis, ullam labore officiis ipsa beatae dolore, assumenda eos harum repudiandae, qui ab! Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
-						</section>
-						<section class="feature 6u$ 12u$(small)">
-							<img class="image fit" src="images/pic04.jpg" alt="" />
-							<h3 class="title">Eveniet, reiciendis, veniam</h3>
-							<p>Rem nulla molestiae inventore quibusdam repudiandae doloremque eveniet ullam, qui autem possimus saepe laudantium numquam sapiente vel. Repudiandae, nihil tempore. Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+							<img class="image fit" src="images/bg2.jpg" alt="" />
+							<h3 class="title">Suasana Study From Home</h3>
+							<p>Suasana study from home saya awalnya biasa saja sih tetapi setelah beberapa bulan mulai terasa bosan. Awalnya saya seneng karna bisa 
+      							  bangun pas pas kelas dimulai dan gak perlu jalan ke kampus. Tetapi setelah sebulan saya merasa bosan dan kurang sosialisasi. Hal 
+     							   yang paling bikin bosen karna gak ada temen buat ngobrol sama becanda. Selain itu belajar dari rumah membuat saya yang awalnya kurang maksimal
+     							   dalam menyerap ilmu makin sulit lagi dalam memahami materi, serta ada yang kurang jika sedang belajar kelompok. Saya berharap pandemi ini cepat selesai
+   							     sehingga saya dapat menikmati suasana kuliah yang lama.</p>
 						</section>
 					</div>
-					<footer>
-						<ul class="actions">
-							<li>
-								<a href="#" class="button alt big">Learn More</a>
-							</li>
-						</ul>
-					</footer>
 				</div>
 			</section>
 
@@ -115,56 +57,10 @@
 			<footer id="footer">
 				<div class="container">
 					<div class="row">
-						<section class="4u 6u(medium) 12u$(small)">
-							<h3>Lorem ipsum</h3>
-							<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda, cumque!</p>
-							<ul class="alt">
-								<li><a href="#">Lorem ipsum dolor sit amet.</a></li>
-								<li><a href="#">Quod adipisci perferendis et itaque.</a></li>
-								<li><a href="#">Itaque eveniet ullam, veritatis reiciendis?</a></li>
-								<li><a href="#">Accusantium repellat accusamus a, soluta.</a></li>
-							</ul>
-						</section>
-						<section class="4u 6u$(medium) 12u$(small)">
-							<h3>Nostrum, repellat!</h3>
-							<p>Tenetur voluptate exercitationem eius tempora! Obcaecati suscipit, soluta earum blanditiis.</p>
-							<ul class="alt">
-								<li><a href="#">Lorem ipsum dolor sit amet.</a></li>
-								<li><a href="#">Id inventore, qui necessitatibus sunt.</a></li>
-								<li><a href="#">Deleniti eum odit nostrum eveniet.</a></li>
-								<li><a href="#">Illum consectetur quibusdam eos corporis.</a></li>
-							</ul>
-						</section>
-						<section class="4u$ 12u$(medium) 12u$(small)">
-							<h3>Contact Us</h3>
-							<ul class="icons">
-								<li><a href="#" class="icon rounded fa-twitter"><span class="label">Twitter</span></a></li>
-								<li><a href="#" class="icon rounded fa-facebook"><span class="label">Facebook</span></a></li>
-								<li><a href="#" class="icon rounded fa-pinterest"><span class="label">Pinterest</span></a></li>
-								<li><a href="#" class="icon rounded fa-google-plus"><span class="label">Google+</span></a></li>
-								<li><a href="#" class="icon rounded fa-linkedin"><span class="label">LinkedIn</span></a></li>
-							</ul>
-							<ul class="tabular">
-								<li>
-									<h3>Address</h3>
-									1234 Somewhere Road<br>
-									Nashville, TN 00000
-								</li>
-								<li>
-									<h3>Mail</h3>
-									<a href="#">someone@untitled.tld</a>
-								</li>
-								<li>
-									<h3>Phone</h3>
-									(000) 000-0000
-								</li>
-							</ul>
-						</section>
+					
 					</div>
 					<ul class="copyright">
-						<li>&copy; Untitled. All rights reserved.</li>
-						<li>Design: <a href="http://templated.co">TEMPLATED</a></li>
-						<li>Images: <a href="http://unsplash.com">Unsplash</a></li>
+						<li>Rahmadhan Adinugroho 11180910000046</li>
 					</ul>
 				</div>
 			</footer>
